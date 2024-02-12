589a99419eac975ece79a92ef6ca02f6fa9d5405
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-02-12 09:39:19 +0530

    routes

---
+++ backend/routes/users.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+let User = require("../models/user");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then((user) => res.json({ user }))
+    .catch((error) => res.status(400).json("Error " + error));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const newUser = new User({ username });
+  newUser
+    .save()
+    .then(() => res.send("User added!"))
+    .catch((error) => res.status(400).json("Error " + error));
+});
+
+module.exports = router;
